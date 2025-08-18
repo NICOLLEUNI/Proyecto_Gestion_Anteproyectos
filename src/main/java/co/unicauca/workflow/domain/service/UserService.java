fb7eda83b06b4479e25cc1dc5ff570ea8ffeff38
NICOLLEUNI <nicollemontano@unicauca.edu.co>
2025-08-17 22:30:00 -0500

    Update UserService-BD

--- src/main/java/co/unicauca/workflow/domain/service/UserService.java
+++ src/main/java/co/unicauca/workflow/domain/service/UserService.java
@@ -14,12 +14,5 @@ public class UserService {
     public void hashPassword(){}
     public void validatePassword(){}
     public void validateEmail(){}
-    private boolean isEmailUnicauca(String email) {
-    return email != null && email.toLowerCase().endsWith("@unicauca.edu.co");
-}
 
-    private boolean isStrongPassword(String pwd) {
-    // ≥6, al menos un dígito, un especial, una mayúscula
-    return pwd != null && pwd.matches("^(?=.*[0-9])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{6,}$");
-}
 }
