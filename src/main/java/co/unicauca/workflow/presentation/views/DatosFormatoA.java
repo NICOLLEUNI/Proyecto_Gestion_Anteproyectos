a516b080edbdd1c719bb45f6605f0608a1ec1afb
NICOLLEUNI <nicollemontano@unicauca.edu.co>
2025-09-29 01:32:58 -0500

    Pass Persona object between panels for user context
    Updated GUIMenuDocente, DatosFormatoA, and AdjuntarDocumentos to pass the Persona object between panels. This ensures that user context is maintained throughout the workflow, allowing panels to access the logged-in user's information as needed.

--- src/main/java/co/unicauca/workflow/presentation/views/DatosFormatoA.java
+++ src/main/java/co/unicauca/workflow/presentation/views/DatosFormatoA.java
@@ -13,6 +13,7 @@ import co.unicauca.workflow.access.IEstudianteRepository;
 import co.unicauca.workflow.access.IFormatoARepository;
 import co.unicauca.workflow.domain.entities.Estudiante;
 import co.unicauca.workflow.domain.entities.FormatoA;
+import co.unicauca.workflow.domain.entities.Persona;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Insets;
@@ -34,9 +35,10 @@ public class DatosFormatoA extends javax.swing.JPanel {
    
       private IFormatoARepository repoFormato = Factory.getInstance().getFormatoARepository("default");
        private IEstudianteRepository repoEstudiantes = Factory.getInstance().getEstudianteRepository("default");
-    
-    public DatosFormatoA() {
+       private Persona persona;
+    public DatosFormatoA(Persona persona) {
         initComponents();
+        this.persona=persona;
         
         boxModalidad.setModel(new javax.swing.DefaultComboBoxModel<>(enumModalidad.values()));
           boxModalidad.setSelectedIndex(-1);
@@ -426,7 +428,7 @@ public class DatosFormatoA extends javax.swing.JPanel {
         }
         formato.setEstudiantes(estudiantes);
 
-        AdjuntarDocumentos panelDocs = new AdjuntarDocumentos(formato);
+        AdjuntarDocumentos panelDocs = new AdjuntarDocumentos(formato,persona);
         showJPanel(panelDocs);
 
     } catch (Exception e) {
